0af2c33fa98d685859f3f4ec29d0149d4588fa87
Lucca Lobo <102260432+Luccacalu@users.noreply.github.com>
2023-10-11 03:21:57 -0300

    feat: added components

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,12 @@
 $("document").ready(function() {
-    $("body").append("<h1>jQuery is working!</h1>");
+    
+    // Carregar a seção de fornecedor
+    $("#fornecedor-section").load("/components/fornecedor/fornecedor.html");
+
+    // Carregar a seção de produtos
+    $("#produtos-section").load("/components/produtos/produtos.html");
+
+    // Carregar a seção de anexos
+    $("#anexos-section").load("/components/anexos/anexos.html");
+
 });
